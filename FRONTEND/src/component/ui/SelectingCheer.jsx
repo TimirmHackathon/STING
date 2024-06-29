ed7e1b5fdc61f302098cfa655bb3a4d9621878a4
jaeiko <jkimkr08@daum.net>
2024-06-29 11:29:04 +0900

    추가: FRONTEND 추가

---
+++ FRONTEND/src/component/ui/SelectingCheer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Select, Space } from "antd";
+
+const options = [
+  { value: "1", label: "응원석에서 일어나서 응원" },
+  { value: "2", label: "무난한 가격에 신날 때만 일어서서 응원" },
+  { value: "3", label: "부담없이 가격이 싼 곳에서 계속 앉아서 응원" },
+  { value: "4", label: "테이블에서 맛있는거 먹으면서 응원" },
+];
+
+const handleChange = (value) => {
+  console.log(`selected ${value}`);
+};
+const SelectingCheer = () => (
+  <Select
+    showSearch
+    placeholder="본인의 응원타입"
+    filterOption={(input, option) =>
+      (option?.label ?? "").toLowerCase().includes(input.toLowerCase())
+    }
+    options={options}
+    onChange={handleChange}
+    style={{ width: "100%" }}
+  />
+);
+export default SelectingCheer;
